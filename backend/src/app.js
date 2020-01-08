f1683e17e323ce5d2c55ab54e17def5fa5af7c1f
williamwjd <william.jose.wjd@gmail.com>
2020-01-08 08:35:51 -0300

    Criando arquivo de rotas e configurando uma rota de teste

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,20 @@
 import express from "express";
 
+import routes from "./routes";
+
 class App {
   constructor() {
     this.server = express();
     this.middleware();
+    this.route();
   }
 
   middleware() {
     this.server.use(express.json());
+  }
+
+  route() {
+    this.server.use(routes);
   }
 }
 
