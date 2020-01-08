5c867967cacbaea5b58d9821ee14edd586177453
williamwjd <william.jose.wjd@gmail.com>
2020-01-08 08:44:03 -0300

    Criando a conexao com o sequelize

---
+++ backend/src/database/index.js
@@ -0,0 +1,11 @@
+import Sequelize from "sequelize";
+
+import config from "../config/database";
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(config);
+  }
+}
+
+export default new Database();
