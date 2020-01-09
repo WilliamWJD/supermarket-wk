d4d258e91c093e7928b66179fbeea0a221113989
williamwjd <william.jose.wjd@gmail.com>
2020-01-09 14:16:10 -0300

    Instanciando o model List

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,8 +3,9 @@ import Sequelize from "sequelize";
 import config from "../config/database";
 
 import User from "../App/Models/User";
+import List from "../App/Models/List";
 
-const models = [User];
+const models = [User, List];
 
 class Database {
   constructor() {
