ed5ad993a705bdefa3c70541f524c24651217906
williamwjd <william.jose.wjd@gmail.com>
2020-01-10 09:01:12 -0300

    Criando metodo para atualizar um usuário

--- backend/src/App/Controllers/UserController.js
+++ backend/src/App/Controllers/UserController.js
@@ -8,6 +8,7 @@ class UserController {
       email: Yup.string()
         .email()
         .required(),
+      image_path: Yup.string(),
       password: Yup.string()
         .required()
         .min(8),
@@ -20,11 +21,66 @@ class UserController {
       return res.status(400).json({ error: "Error on validate schema." });
     }
 
-    const { id, name, email, createdAt, updatedAt } = await User.create(
-      req.body
-    );
+    const {
+      id,
+      name,
+      email,
+      image_path,
+      createdAt,
+      updatedAt
+    } = await User.create(req.body);
 
-    return res.status(201).json({ id, name, email, createdAt, updatedAt });
+    return res
+      .status(201)
+      .json({ id, name, email, image_path, createdAt, updatedAt });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object.shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      image_path: Yup.string(),
+      oldPassword: Yup.string().min(8),
+      password: Yup.string()
+        .min(8)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      passwordConfirmation: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      )
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: "Error on validate schema." });
+    }
+
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const { oldPassword } = req.body;
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: "User password not match." });
+    }
+
+    const {
+      id,
+      name,
+      email,
+      image_path,
+      createdAt,
+      updatedAt
+    } = await user.update(req.body);
+
+    return res
+      .status(201)
+      .json({ id, name, email, image_path, createdAt, updatedAt });
   }
 }
 
