5d4784496df3cbbd3d5ebf34aff5071cbd5de227
williamwjd <william.jose.wjd@gmail.com>
2020-01-20 15:45:15 -0300

    Adicionando coluna file_id na tabela de usuários

---
+++ backend/src/database/migrations/20200120184135-add-column-file-users.js
@@ -0,0 +1,15 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("users", "file_id", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: { model: "files", key: "id" },
+      onUpdate: "CASCADE",
+      onDelete: "SET NULL"
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn("users", "file_id");
+  }
+};
