d8e8b2dc31792a45d358346ba7461196779e4e44
williamwjd <william.jose.wjd@gmail.com>
2020-01-21 11:44:45 -0300

    Criando o SubHeader

---
+++ mobile/src/components/SubHeader/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+
+import {MaterialIcons} from '@expo/vector-icons'
+
+export default function SubHeader(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>Shopping list</Text>
+            <TouchableOpacity style={styles.IconFilter}>
+                <MaterialIcons name="filter-list" size={27} color="#7F8C8D"/>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        marginTop:11,
+    },
+
+    title:{
+        fontSize:21,
+        fontWeight:'bold',
+        color:'#7F8C8D'
+    },
+
+})
--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import {View, Text} from 'react-native'
 
 import Header from '../../components/Header'
+import SubHeader from '../../components/SubHeader'
 
 export default function Main(){
     return(
         <View>
             <Header/>
+            <SubHeader/>
         </View>
     )
 }
